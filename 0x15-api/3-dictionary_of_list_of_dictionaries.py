d99767a0cad0ab338fdfaf1338044635db820962
Ahmad <ahmad.new.m.v@gmail.com>
2024-06-04 04:47:37 +0000

    Removed a bug

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -7,20 +7,21 @@ import sys
 
 if __name__ == "__main__":
     users_dict = {}
-    for id in range(0, 10):
-        u = requests.get(f"https://jsonplaceholder.typicode.com/users/{id}")
-        u = json.loads(u.text)
-        u_tasks = requests.\
+    for id in range(1, 11):
+        user_response = requests.\
+            get(f"https://jsonplaceholder.typicode.com/users/{id}")
+        user = json.loads(user_response.text)
+        user_tasks_response = requests.\
             get(f"https://jsonplaceholder.typicode.com/todos?userId={id}")
-        u_tasks = json.loads(u_tasks.text)
-        user_tasks = []
-        for task in u_tasks:
+        user_tasks = json.loads(user_tasks_response.text)
+        user_tasks_list = []
+        for task in user_tasks:
             task["task"] = task["title"]
             del task["id"]
             del task["title"]
             del task["userId"]
-            task.update({"username": u.get('username')})
-            user_tasks.append(task)
-        users_dict.update({id + 1: user_tasks})
+            task.update({"username": user.get('username')})
+            user_tasks_list.append(task)
+        users_dict.update({id: user_tasks})
     with open("todo_all_employees.json", 'w', encoding="utf-8") as json_file:
         json_file.write(json.dumps(users_dict))
