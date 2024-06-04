bc8619d01cc910108b695b80764c246a9f2a7b63
Ahmad <ahmad.new.m.v@gmail.com>
2024-06-04 02:00:04 +0000

    Added the solution of the 1st task

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""To be updated"""
+import requests
+import json
+import sys
+
+id = sys.argv[1]
+
+if __name__ == "__main__":
+    user = requests.get(f"https://jsonplaceholder.typicode.com/users/{id}")
+    user = json.loads(user.text)
+    user_tasks = requests.\
+        get(f"https://jsonplaceholder.typicode.com/todos?userId={id}")
+    user_tasks = json.loads(user_tasks.text)
+    complete = 0
+    all = 0
+    tasks = []
+    for task in user_tasks:
+        if task.get('completed'):
+            complete += 1
+            tasks.append(task.get('title'))
+        all += 1
+    print(f"Employee {user.get('name')} is done with tasks({complete}/{all}):")
+    for task in tasks:
+        print(f"\t {task}")
