aea453b40bcd09ba9a451edc19619be644c4b872
Ahmad <ahmad.new.m.v@gmail.com>
2024-06-04 04:35:27 +0000

    Improved readability

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -7,19 +7,20 @@ import sys
 
 if __name__ == "__main__":
     id = sys.argv[1]
-    u = requests.get(f"https://jsonplaceholder.typicode.com/users/{id}")
-    u = json.loads(u.text)
-    u_tasks = requests.\
+    user_response = requests.\
+        get(f"https://jsonplaceholder.typicode.com/users/{id}")
+    user = json.loads(user_response.text)
+    user_tasks_response = requests.\
         get(f"https://jsonplaceholder.typicode.com/todos?userId={id}")
-    u_tasks = json.loads(u_tasks.text)
-    user_tasks = []
-    for task in u_tasks:
+    user_tasks = json.loads(user_tasks_response.text)
+    user_tasks_list = []
+    for task in user_tasks:
         task["task"] = task["title"]
         del task["id"]
         del task["title"]
         del task["userId"]
-        task.update({"username": u.get('username')})
-        user_tasks.append(task)
-    user_dict = {id: user_tasks}
+        task.update({"username": user.get('username')})
+        user_tasks_list.append(task)
+    user_dict = {id: user_tasks_list}
     with open(f"{id}.json", 'w', encoding="utf-8") as json_file:
         json_file.write(json.dumps(user_dict))
